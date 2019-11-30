7259f098ca1af595173ff4dac75bed9dd730a105
ganeshgla <54625420+ganeshgla@users.noreply.github.com>
2019-11-30 16:30:53 +0530

    //solution3.py

--- solution3.py
+++ solution3.py
@@ -1,1 +1,8 @@
-
+try:
+    a=int(input())
+except ValueError as e:
+    print(" value error handled",e)
+except EOFError as e:
+    print(" eof error handled",e)
+except KeyboardInterrupt as e:
+    print(" keyboarderror handled",e)
